a5a5ce05bfc91b429a3475de4f0faaf8e7610a9b
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2024-11-13 12:31:19 -0500

    made the RANDOM BACKGROUND!!!!

--- script.js
+++ script.js
@@ -13,7 +13,8 @@
 //IT CAN TALK!!!!!!!!!!!!!!!!!!!
 
 window.onload = function startUp() {
-  prompter();
+  questionBG();
+  setRandomOffset();
 };
 
 function prompter() {
@@ -23,4 +24,65 @@ function prompter() {
   let swallowAirSpeed = prompt("What is the airspeed of an unladen swallow?");
   let myStory = `<p>Hello ${userName}. Answer me these questions three, and the other side you'll see.</p><p>Your quest is to ${userQuest}</p>`;
   document.getElementById("story").innerHTML = myStory;
+}
+
+/*.question {
+      animation-name: fadeInOut;
+      animation-duration: 5s;
+      animation-iteration-count: infinite;
+    }*/
+
+//bg gen
+function setRandomOffset() {
+  let questions = document.querySelectorAll(".question");
+  Array.from(questions);
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].setAttribute(
+      "style",
+      `animation-name: fadeInOut; animation-duration: 8s; animation-iteration-count: infinite; animation-delay: ${
+        Math.random() * 10 - 9
+      }s;`
+    );
+  }
+}
+
+function questionBG() {
+  let bHeight = window.innerHeight;
+  let bg = document.getElementById("bg");
+  let height = 0;
+  while (height < bHeight) {
+    let results = new _row();
+    let subDiv = results[0];
+    let instance = results[1];
+    height = height + subDiv.offsetHeight - 70;
+    let random = Math.round(Math.random() * 50);
+    let sign;
+    Math.random() > 0.5 ? (sign = 1) : (sign = -1);
+    subDiv.setAttribute("style", `margin-left: ${random * sign}px;`);
+  }
+}
+
+function _row() {
+  let questions = [];
+  let bWidth = document.body.offsetWidth;
+  let widths = 0;
+  let bg = document.getElementById("bg");
+  let subdiv = document.createElement("div");
+  subdiv.classList.add("subdiv");
+  bg.append(subdiv);
+  while (widths < bWidth) {
+    let random = Math.round(Math.random() * 100);
+    let question = new _question(random);
+    subdiv.append(question);
+    widths = widths + question.offsetWidth;
+    questions.push(question);
+  }
+  return [subdiv, questions];
+}
+
+function _question(width) {
+  let question = document.createElement("div");
+  question.classList.add("question");
+  question.setAttribute("style", `transform: scale(${width}%)`);
+  return question;
 }
